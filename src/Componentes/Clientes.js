49dd8aa9436f2c690cb3295cc79efb1bccec0fb0
Mario <canalessanchezmario@gmail.com>
2023-09-17 10:37:44 +0200

    Se añade error por si no se encuentra la api o si devuelve error se muestre un mensaje en pantalla

--- src/Componentes/Clientes.js
+++ src/Componentes/Clientes.js
@@ -5,6 +5,7 @@ import Loader from './Loader';
 export const Clientes = () => {
     const [clientes, setClientes] = useState([]);
     const [showLoader, setShowLoader] = useState(true);
+    const [errorApi, setErrorApi] = useState(false)
     const location = useLocation();
 
     function eliminarCliente(idCliente) {
@@ -29,7 +30,7 @@ export const Clientes = () => {
                alert("Usuario eliminado")
               
             })
-            .catch((error) => console.log(error + "Error en la llamada"));
+            .catch(() => setErrorApi(true));
 
 
            
@@ -49,66 +50,76 @@ export const Clientes = () => {
         .then((data) => {
           setClientes(data);
           setShowLoader(false);
-        });
+        })
+        .catch(() => setErrorApi(true));
     }, [location.pathname, clientes]);
 
     return (
       <>
         {showLoader ? (
-          <Loader setShowLoader={setShowLoader}/>
+          <Loader setShowLoader={setShowLoader} />
         ) : (
           <div>
-            <div style={{padding:'2%'}}>
-              <Link to="/NuevoCliente"  className="button btn-nuevo">
-                Nuevo cliente
-              </Link>
-            </div>
-            <div className='table-container'>
-              <table border={1}>
-                <thead>
-                  <tr>
-                    <td>IdCliente</td>
-                    <td>email</td>
-                    <td>Nombre</td>
-                    <td>Apellido</td>
-                    <td>Empresa</td>
-                    <td>fecha creación</td>
-                    <td>País</td>
-                    <td>Acciones</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {clientes.map((item, i) => {
-                    return (
-                      <tr key={i}>
-                        <td key={item.idCliente}>{item.idCliente}</td>
-                        <td>{item.email}</td>
-                        <td>{item.nombre}</td>
-                        <td>{item.apellido}</td>
-                        <td>{item.empresa}</td>
-                        <td>{item.fechaCreacion}</td>
-                        <td>{item.pais}</td>
-                        <td>
-                          <Link
-                            to={`/EditarCliente/${item.idCliente}`}
-                            state={{ from: item.idCliente }}
-                            className="button btn-warning"
-                          >
-                            Editar cliente
-                          </Link>
-                          <button
-                            className="button btn-danger"
-                            onClick={() => eliminarCliente(item.idCliente)}
-                          >
-                            Eliminar Cliente
-                          </button>
-                        </td>
+            {errorApi ? (
+              <div className='centered-container'>
+                <h1>Hay un problema con la recuperación de los datos.</h1>
+                <h2>No se puede acceder a la API</h2>
+              </div>
+            ) : (
+              <div>
+                <div style={{ padding: "2%" }}>
+                  <Link to="/NuevoCliente" className="button btn-nuevo">
+                    Nuevo cliente
+                  </Link>
+                </div>
+                <div className="table-container">
+                  <table border={1}>
+                    <thead>
+                      <tr>
+                        <td>IdCliente</td>
+                        <td>email</td>
+                        <td>Nombre</td>
+                        <td>Apellido</td>
+                        <td>Empresa</td>
+                        <td>fecha creación</td>
+                        <td>País</td>
+                        <td>Acciones</td>
                       </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+                    </thead>
+                    <tbody>
+                      {clientes.map((item, i) => {
+                        return (
+                          <tr key={i}>
+                            <td key={item.idCliente}>{item.idCliente}</td>
+                            <td>{item.email}</td>
+                            <td>{item.nombre}</td>
+                            <td>{item.apellido}</td>
+                            <td>{item.empresa}</td>
+                            <td>{item.fechaCreacion}</td>
+                            <td>{item.pais}</td>
+                            <td>
+                              <Link
+                                to={`/EditarCliente/${item.idCliente}`}
+                                state={{ from: item.idCliente }}
+                                className="button btn-warning"
+                              >
+                                Editar cliente
+                              </Link>
+                              <button
+                                className="button btn-danger"
+                                onClick={() => eliminarCliente(item.idCliente)}
+                              >
+                                Eliminar Cliente
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </>
